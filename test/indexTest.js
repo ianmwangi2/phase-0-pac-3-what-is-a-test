e450a7df5cb37764c5b7eff0c67b6adeee1ca1f0
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-06-20 15:12:34 +0300

    modified Js files

--- test/indexTest.js
+++ test/indexTest.js
@@ -29,10 +29,21 @@ describe('what-is-a-test', () => {
 // The code below ensures that students who are using CodeGrade will get credit 
 // for the code-along in Canvas; you can disregard it.
 
-describe('', () => {
-  describe('', () => {
-    it('', () => {
-      return true;
-    })
-  })
-})
+describe("Name", () => {
+  it('returns "Susan"', () => {
+    expect(name).toEqual("Susan");
+  });
+});
+
+describe("Height", () => {
+  it("is less than 40", () => {
+    expect(height).toBeLessThan(40);
+  });
+});
+
+describe("Message", () => {
+  it("gives the name and height", () => {
+    expect(message).toInclude(name);
+    expect(message).toInclude(height);
+  });
+});
